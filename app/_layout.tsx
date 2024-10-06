994400b98dd17280ae794ef8c26c702ac22878ce
BryanFonseca <hello@bryanfonseca.dev>
2024-10-06 00:37:03 -0500

    config(App): Add routing support;

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+function Layout() {
+    return (
+        <Stack
+            screenOptions={{
+                contentStyle: {
+                    padding: "5%",
+                },
+            }}
+        />
+    );
+}
+
+export default Layout;
---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function Page() {
+    return <Text>Main Page</Text>;
+}
+
+export default Page;
