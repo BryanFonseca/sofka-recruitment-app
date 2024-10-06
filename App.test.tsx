b33c67a21e053b190d4a5f8bf19994444f4d6a43
BryanFonseca <hello@bryanfonseca.dev>
2024-10-06 10:24:26 -0500

    config(App): Remove old root component;

--- App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react-native";
-import App from "./App";
-
-describe("<App />", () => {
-    it("should be true", () => {
-        render(<App />);
-        screen.getByText("Open up App.tsx to start working on your app!");
-    });
-});
